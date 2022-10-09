e1e72334e8b92cfa5bb0301774ff5ea86cfe440c
BuildTools <unconfigured@null.spigotmc.org>
2022-10-09 19:50:26 +0200

    updated

--- InimcoCase/Program/scr/App/scripts/Dropdown.js
+++ InimcoCase/Program/scr/App/scripts/Dropdown.js
@@ -29,5 +29,43 @@ function AddSkillToList(value) {
   }
   var node = document.createElement('li');
   node.appendChild(document.createTextNode(value.innerText));
+  node.addEventListener('click', function() {
+    this.parentNode.removeChild(this);
+  })
   document.querySelector('ul').appendChild(node);
 }
+
+const AddSkillBtn = document.getElementById("AddSkillBtn");
+
+AddSkillBtn.addEventListener("click", function(){
+  const SkillTxt = document.getElementById("AddSKill-Textbox")
+  if (SkillTxt.value == ""){
+    alert("please enter a skill to add");
+    return;
+  }
+  const li = document.querySelectorAll('ul li');
+  for (let i = 0; i < li.length; i++) {
+    // it is the same as equal to
+    if (li[i].textContent === SkillTxt.value) {
+        alert("That skill is already in the list!")
+        return;
+    }
+  }
+  var node = document.createElement('li');
+  node.appendChild(document.createTextNode(SkillTxt.value));
+  node.addEventListener('click', function() {
+    this.parentNode.removeChild(this);
+  })
+  document.querySelector('ul').appendChild(node);
+})
+
+
+$(document).ready(function(){
+  $('#AddSKill-Textbox').keypress(function(e){
+    if(e.keyCode==13)
+    $('#AddSkillBtn').click();
+  });
+});
+
+
+
