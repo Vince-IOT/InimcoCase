dbf3086f9052206666ba3809066e8eaf7c4a5d54
BuildTools <unconfigured@null.spigotmc.org>
2022-10-09 14:32:31 +0200

    Update

--- InimcoCase/Program/scr/App/scripts/Dropdown.js
+++ InimcoCase/Program/scr/App/scripts/Dropdown.js
@@ -1,19 +1,25 @@
 function myFunction() {
-    document.getElementById("SkillsDropdown").classList.toggle("show");
-  }
-  
-  function filterFunction() {
-    var input, filter, ul, li, a, i;
-    input = document.getElementById("SKillsInput");
-    filter = input.value.toUpperCase();
-    div = document.getElementById("SkillsDropdown");
-    a = div.getElementsByTagName("a");
-    for (i = 0; i < a.length; i++) {
-      txtValue = a[i].textContent || a[i].innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        a[i].style.display = "";
-      } else {
-        a[i].style.display = "none";
-      }
+  document.getElementById("SkillsDropdown").classList.toggle("show");
+}
+
+function filterFunction() {
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("SKillsInput");
+  filter = input.value.toUpperCase();
+  div = document.getElementById("SkillsDropdown");
+  a = div.getElementsByTagName("a");
+  for (i = 0; i < a.length; i++) {
+    txtValue = a[i].textContent || a[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "";
+    } else {
+      a[i].style.display = "none";
     }
   }
+}
+
+Skill = document.getElementById("Skill").addEventListener("click", AddSkillToList)
+
+function AddSkillToList() {
+  alert("Test")
+}
